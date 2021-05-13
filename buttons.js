19b2ba10ef39132b67a2359b0bff21cc6bd6d5a0
Pignuuu <Pignuuu@gmail.com>
2021-05-14 00:52:50 +0200

    Add bingbong folder for fun

---
+++ buttons.js
@@ -0,0 +1,34 @@
+! function() {
+    var x = document.getElementById("hideDiv2");
+
+    x.style.display = "none";
+
+    // Do processing here	
+}()
+
+function toggleProjects2() {
+    var x = document.getElementById("hideDiv2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleProjects() {
+    var x = document.getElementById("hideDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleProjects2() {
+    var x = document.getElementById("hideDiv2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
